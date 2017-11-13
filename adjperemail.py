39cf57b205c3eaaf55a365d53754a91938627e82
Vighnesh Chenthil Kumar <vighneshck97@gmail.com>
2017-11-13 19:31:37 +0530

    Adds code and output for adjectives per email for each author

--- adjperemail.py
+++ adjperemail.py
@@ -7,15 +7,12 @@ import nltk
 
 classes = ['Benjamin_Rogers','Chris_Dorland','Drew_Fossum','Jeffrey_Shankman','Kevin_Presto','Kimberly_Watson','Lynn_Blair','Mark_Haedicke','Michelle_Cash', 'Phillip_Allen']
 #classes = ['Benjamin_Rogers']
-#print len(classes)
 
 direc = sys.argv[1]
 #direc = "./clean_enron/"
 
 for c in classes:
 	listing = os.listdir(direc+c)
-	#numemail = len(listing)
-	#count = 0
 	for filename in listing:
 		f = open(direc+c+'/'+filename, 'r')
 		text = f.read()
